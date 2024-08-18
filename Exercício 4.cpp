d93e973e13e2c2451ba66f80151f1636e92dfb81
João Pedro Vidal <156215754+joaopedrovidal@users.noreply.github.com>
2024-08-18 14:02:48 -0300

    Update Exercício 4.cpp

--- Exercício 4.cpp
+++ Exercício 4.cpp
@@ -1,3 +1,23 @@
+// Exercício 4:
 //
-// Created by João Pedro Vidal on 18/08/2024.
-//
+// Construir um programa que calcule e apresente o resultado da conversão de 0 a 100  graus Fahrenheit para graus Celsius.
+// O cálculo dessa conversão deve ficar isolado em  uma função estar de acordo com a seguinte fórmula: C=5/9.(F-32)
+
+#include <iostream>
+using namespace std;
+
+
+float temperatura(float f) {
+    float c;
+    c = f * (5.0/9.0) - 32.0;
+    return c;
+}
+
+int main () {
+    float f,c;
+    for (f = 0; f <= 100; f++) {
+        c = temperatura(f);
+        cout << f << " graus Fahrenheit equivalem a " << c << " graus Celsius " << endl;
+    }
+    return 0;
+}
